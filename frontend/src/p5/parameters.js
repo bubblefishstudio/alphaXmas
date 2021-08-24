d739c410f9b9ed7382999d3b54315fded4d60cd5
Matteo Bernardini <mat_teobe@yahoo.it>
2021-08-24 13:28:21 +0200

    parametric L-system

--- frontend/src/p5/parameters.js
+++ frontend/src/p5/parameters.js
@@ -1,69 +1,67 @@
+import { Grammar } from "./model.js";
+
 export const commands = new Map([
-	["F", (s) => {
+	["F", (s, n=1) => {
 		if (s.glue > -1) {
-			s.turtle.move(s.len);
+			s.turtle.move(s.len*n);
 			s.vtx[s.glue] = s.turtle.position;
 		} else {
 			s.vtx.push(s.turtle.position);
-			s.turtle.move(s.len);
+			s.turtle.move(s.len*n);
 			s.vtx.push(s.turtle.position);
 			s.glue = s.vtx.length-1;
 		}
 	}],
-	["f", (s) => {
+	["f", (s, n=1) => {
 		s.glue = -1;
-		s.turtle.move(s.len);
+		s.turtle.move(s.len*n);
 	}],
-	["+", (s) => {
+	["+", (s, n) => {
 		s.glue = -1;
-		s.turtle.yaw(s.angle);
+		s.turtle.yaw(n);
 	}],
-	["-", (s) => {
+	["-", (s, n) => {
 		s.glue = -1;
-		s.turtle.yaw(-s.angle);
+		s.turtle.yaw(-n);
 	}],
-	["^", (s) => {
+	["^", (s, n) => {
 		s.glue = -1;
-		s.turtle.pitch(s.angle);
+		s.turtle.pitch(n);
 	}],
-	["&", (s) => {
+	["&", (s, n) => {
 		s.glue = -1;
-		s.turtle.pitch(-s.angle);
+		s.turtle.pitch(-n);
 	}],
-	["/", (s) => {
-		s.turtle.roll(s.angle);
+	["/", (s, n) => {
+		s.turtle.roll(n);
 	}],
-	["\\", (s) => {
-		s.turtle.roll(-s.angle);
+	["\\", (s, n) => {
+		s.turtle.roll(-n);
 	}],
-	["|", (s) => {
+	["|", (s, n) => {
 		s.glue = -1;
 		s.turtle.flip();
 	}],
-	["[", (s) => {
+	["[", (s, n) => {
 		s.stack.push([s.turtle.copy(), s.glue]);
 		s.glue = -1;
 	}],
-	["]", (s) => {
+	["]", (s, n) => {
 		[s.turtle, s.glue] = s.stack.pop();
 	}],
-	["!", (s) => {
+	["!", (s, n) => {
 		s.len *= 0.9;
 	}],
-	["?", (s) => {
+	["?", (s, n) => {
 		s.len /= 0.9;
-	}],
-	[".", (s) => {
-		s.glue = -1;
-		s.turtle.pitch(3);
 	}],
 ]);
 
-export const grammar_axiom = "FFFT";
+export const grammar_axiom = Grammar.parse("F3 T");
 
 export const grammar_rules = new Map([
-	["T",  "/!FFBBBBBBT"],
-	["B",  "////[|^^^FY]"],
-	["Y",  "Z!Y?"],
-	["Z",  ".[/--FFF][\\++FFF][&&FFF]F"],
+	["T",  (_) => Grammar.parse("/15 ! F2 B B B B B B T")],
+	["B",  (_) => Grammar.parse("/60 [ | ^45 F Y ]")],
+	["Y",  (_) => Grammar.parse("Z ! Y ?")],
+	["Z",  (_) => Grammar.parse("^5 [ /15 -30 F3 ] [ \\15 +30 F3 ] [ &30 F3 ] F")],
 ]);
