84a6238981f3e3cb37148e84bc2b8e8e3c146514
Matteo Bernardini <mat_teobe@yahoo.it>
2021-08-27 16:08:16 +0200

    fix: future

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -3,4 +3,4 @@
 export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 // asyncify any function
-export const future = (f) => new Promise(resolve => {f(); resolve()});
+export const future = (f) => new Promise(resolve => resolve(f()));
