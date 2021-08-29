3c359a2cbc30df9fe297767dade02f3e3e2de5a6
mttbernardini <mttbernardini@users.noreply.github.com>
2021-08-29 09:33:50 +0000

    deploy: 36ea54cab9c25db3bd142257a556007428979ae1

--- utils.js
+++
@@ -1,6 +0,0 @@
-
-// async sleep
-export const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-// asyncify any function
-export const future = (f) => new Promise(resolve => {f(); resolve()});
