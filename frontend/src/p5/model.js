6794bd1d5672cf476af69cc9c4f8a15a81127ce1
Matteo Bernardini <mat_teobe@yahoo.it>
2021-09-20 14:48:43 +0200

    draw star (todo: draw only on cadence)

--- frontend/src/p5/model.js
+++ frontend/src/p5/model.js
@@ -13,11 +13,14 @@ export class Tree {
 		this._leaf_geom = new p5.Geometry();
 		this._lights_vtx = new p5.Geometry();
 		this._lights_colors = ["red", "yellow", "blue", "purple"];
+		this._star_position = new p5.Vector(0,0,0);
+
+		this.add_star(); // debug
 	}
 
 	_compile() {
 		let state = {
-			len: this._cvs.height / 20,
+			len: this._cvs.height / 21,
 			angle: 15,
 			vtx: [],
 			leaf_vtx: [],
@@ -25,6 +28,7 @@ export class Tree {
 			stack: [],
 			turtle: new Turtle(),
 			glue: -1,
+			star: new p5.Vector(0,0,0),
 		};
 
 		// compute vertices from turtle movements
@@ -54,6 +58,9 @@ export class Tree {
 		// tree lights
 		this._lights_vtx = state.lights.map(v => v.add(0,0,-3));
 
+		// tree star position
+		this._star_position = state.star;
+
 		return this;
 	}
 
@@ -71,7 +78,7 @@ export class Tree {
 	}
 
 	add_star() {
-		// TODO
+		this._star = new Star(this._cvs, 30, 8, 5);
 	}
 
 	draw() {
@@ -94,5 +101,78 @@ export class Tree {
 			p.stroke(this._lights_colors[i % this._lights_colors.length]);
 			p.point(this._lights_vtx[i]);
 		}
+
+		// draw star
+		if (this._star !== undefined) {
+			this._star.draw(this._star_position);
+		}
+	}
+}
+
+
+class Star {
+	constructor(cvs, width, depth, tips = 5) {
+		this._cvs = cvs;
+		this.tips = tips;
+		this.width = width;
+		this.depth = depth;
+		this._compile();
+	}
+
+	_compile() {
+		const g = new p5.Geometry();
+		const w = this.width;
+
+		// add vertices
+		g.vertices = [new p5.Vector(0, 0, this.depth), new p5.Vector(0, 0, -this.depth)];
+
+		for (let i = 1; i <= this.tips; i++) {
+			g.vertices.push(new p5.Vector(
+				w/2 * Math.cos((i - 1/2) * 2*Math.PI/this.tips),
+				w/2 * Math.sin((i - 1/2) * 2*Math.PI/this.tips),
+				0)
+			); // star wedge
+			g.vertices.push(new p5.Vector(
+				w * Math.cos(i * 2*Math.PI/this.tips),
+				w * Math.sin(i * 2*Math.PI/this.tips),
+				0)
+			); // star tip
+		}
+
+		// add faces
+		for (let i = 0; i < 2*this.tips; i += 2) {
+			// front
+			g.faces.push([0, 2+(i),   2+(i+1)]); // right side
+			g.faces.push([0, 2+(i+1), 2+(i+2) % (2*this.tips)]); // left side (and wrapping back)
+			// back
+			g.faces.push([1, 2+(i),   2+(i+1)]); // right side
+			g.faces.push([1, 2+(i+1), 2+(i+2) % (2*this.tips)]); // left side (and wrapping back)
+		}
+
+		// compute normals
+		g.computeNormals();
+
+		// save geometry
+		g.gid = "star";
+		this.geometry = g;
+	}
+
+	draw(pos) {
+		const p = this._cvs;
+
+		p.translate(pos); // go up
+
+		// make star holder
+		p.stroke("#f7f704");
+		p.strokeWeight(2);
+		p.line(0,0,0, 0,0,this.width);
+		p.translate(0,0,this.width);
+
+		// place star
+		p.rotateZ(Math.PI/this.tips); // align wedge with x-axis
+		p.rotateY(-Math.PI/2); // put star vertically
+		p.fill("#f7f704");
+		p.strokeWeight(0.01);
+		p.model(this.geometry);
 	}
 }
